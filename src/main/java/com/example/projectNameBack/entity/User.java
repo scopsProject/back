03dbc0bfb1e82b6ea106b3920bdc64fb2b459931
yoobin kim <yoobin0821@naver.com>
2025-12-13 02:17:21 +0900

    fix/songsession엔티티변경

--- src/main/java/com/example/projectNameBack/entity/User.java
+++ src/main/java/com/example/projectNameBack/entity/User.java
@@ -1,6 +1,7 @@
 package com.example.projectNameBack.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -12,6 +13,7 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class User {
 
     @Id
